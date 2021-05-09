e713ac3479bae4698a973bf73dd83eb6e27d7fa2
ExLZyor <brice.vachez@insa-lyon.fr>
2021-05-09 02:34:45 +0200

    ItemFactory tests

--- TaintedCainApp/TaintedCainApp/ItemFactory.cs
+++ TaintedCainApp/TaintedCainApp/ItemFactory.cs
@@ -13,23 +13,23 @@ namespace TaintedCainApp
             #region Exceptions
             if (_name == null)
             {
-                throw new ArgumentNullException("Argument _name cannot be null.");
+                throw new ArgumentNullException("Parameter _name cannot be null.");
             }
 
             if(_name == String.Empty)
             {
-                throw new ArgumentException("Attribute _name cannot be empty.");
+                throw new ArgumentException("Parameter _name cannot be empty.");
             }
 
             if(_recipes == null)
             {
-                throw new ArgumentNullException("Argument _recipes cannot be null.");
+                throw new ArgumentNullException("Parameter _recipes cannot be null.");
             }
 
             foreach(Recipe recipe in _recipes) {
                 if(!recipe.IsValid())
                 {
-                    throw new ArgumentException("One of the recîpe provided to item was not valid.\n" +
+                    throw new ArgumentException("One of the recîpe provided to item was not valid.\n\nRecipe :\n" +
                         recipe.ToString());
                 }
             }

9a8b131274ca8c959a2d01d2170e546294fca863
ExLZyor <brice.vachez@insa-lyon.fr>
2021-05-11 23:05:16 +0200

    Pick Up counting

--- TaintedCainApp/TaintedCainApp/MainAppWindow.Designer.cs
+++ TaintedCainApp/TaintedCainApp/MainAppWindow.Designer.cs
@@ -30,6 +30,8 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainAppWindow));
             this.library = new System.Windows.Forms.Button();
+            this.pickUpCounterPanel = new TaintedCainApp.PickUpCounterPanel();
+            this.generateButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // library
@@ -42,11 +44,32 @@
             this.library.UseVisualStyleBackColor = true;
             this.library.Click += new System.EventHandler(this.library_Click_Click);
             // 
+            // pickUpCounterPanel
+            // 
+            this.pickUpCounterPanel.AutoScroll = true;
+            this.pickUpCounterPanel.Location = new System.Drawing.Point(75, 62);
+            this.pickUpCounterPanel.Name = "pickUpCounterPanel";
+            this.pickUpCounterPanel.Size = new System.Drawing.Size(432, 295);
+            this.pickUpCounterPanel.TabIndex = 1;
+            this.pickUpCounterPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(400, 375);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(87, 23);
+            this.generateButton.TabIndex = 2;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
             // MainAppWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.pickUpCounterPanel);
             this.Controls.Add(this.library);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainAppWindow";
@@ -59,5 +82,7 @@
         #endregion
 
         private System.Windows.Forms.Button library;
+        private PickUpCounterPanel pickUpCounterPanel;
+        private System.Windows.Forms.Button generateButton;
     }
 }
